c3c88500732c12c63874d6399bee5d42fd9a6468
aayushkumar20 <aayushkumarjsr16@protonmail.com>
2023-11-13 23:01:06 +0530

    Initial Commit

---
+++ Swipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swipe
+//
+//  Created by Aayush kumar on 13/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swipe/SwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeApp.swift
+//  Swipe
+//
+//  Created by Aayush kumar on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
